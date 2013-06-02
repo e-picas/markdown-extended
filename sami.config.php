9f0e574712a5c9a28a096e14da9a0dabfb5e14e0
Piero Wbmstr <me@e-piwi.fr>
2013-06-02 22:17:35 +0200

    New config for the Sami documentation generator

---
+++ sami.config.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Application documentation builder
+ *
+ * See https://github.com/fabpot/Sami
+ *
+ * To build doc, run:
+ *     $ php vendor/sami/sami/sami.php render sami.config.php
+ *
+ * To update it, run:
+ *     $ php vendor/sami/sami/sami.php update sami.config.php
+ *
+ */
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in(__DIR__.'/src')
+;
+
+$options = array(
+    'title'                => 'Markdown Extended',
+    'build_dir'            => __DIR__.'/phpdoc',
+    'cache_dir'            => __DIR__.'/../tmp/cache/markdown-extended',
+    'default_opened_level' => 2,
+);
+
+return new Sami($iterator, $options);
+
+// Endfile
