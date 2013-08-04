6c838f92748d805e21468d73474e8ceed18cfb83
Piero Wbmstr <me@e-piwi.fr>
2013-08-04 19:18:46 +0200

    New demo + console models

--- demo/index.php
+++ demo/index.php
@@ -1,10 +1,20 @@
 <?php
-// show errors at least initially
-@ini_set('display_errors','1'); @error_reporting(E_ALL ^ E_NOTICE);
+/**
+ * Show errors at least initially
+ *
+ * `E_ALL` => for hard dev
+ * `E_ALL & ~E_STRICT` => for hard dev in PHP5.4 avoiding strict warnings
+ * `E_ALL & ~E_NOTICE & ~E_STRICT` => classic setting
+ */
+//@ini_set('display_errors','1'); @error_reporting(E_ALL);
+//@ini_set('display_errors','1'); @error_reporting(E_ALL & ~E_STRICT);
+@ini_set('display_errors','1'); @error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 
-// set a default timezone to avoid PHP5 warnings
-$dtmz = date_default_timezone_get();
-date_default_timezone_set( !empty($dtmz) ? $dtmz:'Europe/Paris' );
+/**
+ * Set a default timezone to avoid PHP5 warnings
+ */
+$dtmz = @date_default_timezone_get();
+date_default_timezone_set($dtmz?:'Europe/Paris');
 
 // arguments settings
 $doc = isset($_GET['doc']) ? $_GET['doc'] : null;
