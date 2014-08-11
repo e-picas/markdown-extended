4004357da23986316af7511079af51f8ad2d7460
Piero Wbmstr <me@e-piwi.fr>
2014-08-11 15:13:51 +0200

    do not verify the link IS an url (no protocol)

--- src/MarkdownExtended/Grammar/Filter/AutoLink.php
+++ src/MarkdownExtended/Grammar/Filter/AutoLink.php
@@ -35,7 +35,7 @@ class AutoLink
      */
     public function transform($text)
     {
-        $text = preg_replace_callback('{<((https?|ftp|dict):[^\'">\s]+)>}i',
+        $text = preg_replace_callback('{<([^\'">\s]+)>}i',
             array($this, '_url_callback'), $text);
 
         // Email addresses: <address@domain.foo>
