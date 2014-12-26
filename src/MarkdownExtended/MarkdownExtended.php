5e41b32725683cc8376905f41dcd68b4619fde15
piwi <me@e-piwi.fr>
2014-12-26 23:01:09 +0100

    upgrading to v0.1-gamma4

--- src/MarkdownExtended/MarkdownExtended.php
+++ src/MarkdownExtended/MarkdownExtended.php
@@ -87,8 +87,8 @@ final class MarkdownExtended
      * Class infos ; can be automatically updated by `pre-commit-hook.sh`
      */
     const MDE_NAME      = 'PHP Markdown Extended';
-    const MDE_VERSION   = '0.1-gamma';
-    const MDE_DATE      = '12-06-2014';
+    const MDE_VERSION   = '0.1-gamma4';
+    const MDE_DATE      = '26-12-2014';
     const MDE_SOURCES   = 'http://github.com/piwi/markdown-extended';
 
     /**
