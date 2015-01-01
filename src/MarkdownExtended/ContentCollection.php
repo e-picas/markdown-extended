d278a8f5bdd3f77262cb09a2e06de51330526e6e
piwi <me@e-piwi.fr>
2015-01-01 23:32:11 +0100

    update the scripts headers passing a line

--- src/MarkdownExtended/ContentCollection.php
+++ src/MarkdownExtended/ContentCollection.php
@@ -7,6 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace MarkdownExtended;
 
 use \MarkdownExtended\Helper as MDE_Helper;
