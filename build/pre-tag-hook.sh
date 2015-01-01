c69611c238a1a57acc4797af5688ee8ac6739055
piwi <me@e-piwi.fr>
2015-01-01 23:28:19 +0100

    happy new year ;)

--- build/pre-tag-hook.sh
+++ build/pre-tag-hook.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 #
+# This file is part of the PHP-MarkdownExtended package.
+#
+# (c) Pierre Cassat <me@e-piwi.fr> and contributors
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+
+#
 # An example hook script called before version tag creation.
 # This will receive three arguments:
 # - $1: the project path to work on
