d033a27ea3327f84fd682689b339b676e3e0cc70
piwi <me@e-piwi.fr>
2015-01-03 01:01:55 +0100

    add the documentation on version-tag updates

--- build/make-version.sh
+++ build/make-version.sh
@@ -29,8 +29,9 @@ _DATE="${2:-$(git log -1 --format="%ci" --date=short | cut -s -f 1 -d ' ')}"
 
 sed -i -e "s|const MDE_VERSION.*= '.*'|const MDE_VERSION = '${VERSION}'|;s|const MDE_DATE.*= '.*'|const MDE_DATE = '${_DATE}'|" "$MDE_PHP" \
     && sed -i -e "s|^Version: .*$|Version: ${VERSION}|;s|^Date: .*$|Date: ${_DATE}|" "$MDMAN_FILE" \
+    && sed -i -e "s|^Version: .*$|Version: ${VERSION}|;s|^Date: .*$|Date: ${_DATE}|" "$MDDOC_FILE" \
     && ${HERE}/make-manpage.sh \
-    && echo "version number updated in '${MDE_PHP}' and '${MDMAN_FILE}' and manpages regenerated" \
+    && echo "version number updated in '${MDE_PHP}', '${MDDOC_FILE}' and '${MDMAN_FILE}' and manpages regenerated" \
     || echo "an error occurred!" ;
 
 exit 0
